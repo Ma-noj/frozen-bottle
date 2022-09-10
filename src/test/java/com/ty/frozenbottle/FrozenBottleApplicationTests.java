c2f1b813eb8dc69fd56fba459009f096fe34bb13
unknown <cmoinahmed@gmail.com>
2022-09-10 18:53:31 +0530

    Frozen-bottle

---
+++ src/test/java/com/ty/frozenbottle/FrozenBottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.frozenbottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrozenBottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
