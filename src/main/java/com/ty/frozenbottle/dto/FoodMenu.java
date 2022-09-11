990f4714ab233a05710f55601cf813c244e38f7a
unknown <cmoinahmed@gmail.com>
2022-09-11 10:48:59 +0530

    Moin Added Dto

---
+++ src/main/java/com/ty/frozenbottle/dto/FoodMenu.java
@@ -0,0 +1,29 @@
+package com.ty.frozenbottle.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class FoodMenu {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@OneToMany(mappedBy = "menu")
+	private List<Product> product;
+
+	@OneToOne
+	@JoinColumn
+	private Admin admin;
+
+}
